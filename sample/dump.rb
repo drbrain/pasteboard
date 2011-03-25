1b69375eccc2c43b5b9cc3073e70e5956881fa83
Eric Hodel <drbrain@segment7.net>
2011-03-25 10:13:02 -0700

    Script to dump the clipboard and its encoding

---
+++ sample/dump.rb
@@ -0,0 +1,16 @@
+require 'pasteboard'
+require 'pp'
+
+pb = Pasteboard.new
+
+pb.each.with_index do |item, index|
+  puts "item #{index}"
+  item.map do |flavor, data|
+    puts "flavor: #{flavor}"
+    puts "encoding: #{flavor.encoding} valid: #{flavor.valid_encoding?}" if
+      defined?(Encoding)
+    puts data.inspect[0, 75]
+    puts
+  end
+  puts
+end
